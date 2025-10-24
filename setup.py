589162491bbe4e2554259799807138174742e6a7
appleflyerv3 <164886846+appleflyerv3@users.noreply.github.com>
2025-10-25 04:46:04 +0800

    Revert "feat: cythonize?"
    This reverts commit b5b7043084730483b6c3ae8e064f70ea67bd28bd.
    still slow as hell

--- setup.py
+++ setup.py
@@ -1,20 +1,12 @@
 from setuptools import setup, find_packages
-from Cython.Build import cythonize
-from pathlib import Path
-
-py_files = [str(p) for p in Path("ot_dsim").rglob("*.py")]
 
 setup(
     name='ot_dsim',
     version='0.1.0',
-    packages=find_packages(),
+    packages=['ot_dsim', 'ot_dsim.bignum_lib'],
+    package_dir={'ot_dsim': '.'},
     install_requires=[
         'pycryptodome',
         'tabulate',
     ],
-    ext_modules=cythonize(
-        py_files,
-        compiler_directives={'language_level': "3"},
-    ),
-    zip_safe=False,
 )
